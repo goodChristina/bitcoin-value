fd51149796211f198cce41708a479914c3ff542e
Christina R <137301175+goodChristina@users.noreply.github.com>
2024-03-30 00:12:56 +0000

    Update bitcoin_value.py
    added typewriter effect

--- bitcoin_value.py
+++ bitcoin_value.py
@@ -3,6 +3,8 @@ Query Parameters for coinbase api
 """
 
 import requests as r
+import sys
+from time import sleep
 
 url = "https://api.coinbase.com/v2/exchange-rates?currency=BTC"
 
@@ -12,6 +14,15 @@ resp = r.get(url)
 us_bitcoin = float(resp.json()["data"]["rates"]["USD"])
 uk_bitcoin = float(resp.json()["data"]["rates"]["GBP"])
 
-print(
-    f"\nIf you had bitcoin, each one would be worth ${us_bitcoin:.2f}, or £{uk_bitcoin:.2f}.\n"
-)
+message = f"\nIf you had bitcoin, each one would be worth ${us_bitcoin:.2f}, or £{uk_bitcoin:.2f}.\n"
+
+
+def type_bitcoin(text, delay=0.05):
+    for char in text:
+        sys.stdout.write(char)
+        sys.stdout.flush()
+        sleep(delay)
+    print()
+
+
+type_bitcoin(message)
